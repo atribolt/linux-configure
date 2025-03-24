6cfb8cee00bcec8550eeb40d3d97246ab3a13fa0
atribolt <atribolt@gmail.com>
2025-03-25 00:08:06 +0300

    Fix
    - fixed prompt venv for MacOS
    - edit template for commit
    - add status for submodules into config git

--- .oh-my-zsh/plugins/pyvenv/pyvenv.plugin.zsh
+++ .oh-my-zsh/plugins/pyvenv/pyvenv.plugin.zsh
@@ -4,7 +4,7 @@ PYVENV_NOVENV=''
 
 function pyvenv_prompt_info() {
   if [ -n "$VIRTUAL_ENV" ]; then
-    local PYTHON_VER="$(python --version | grep -E '[0-9]+\.[0-9]+\.[0-9]+' -o)"
+    local PYTHON_VER="$(${VIRTUAL_ENV}/bin/python --version | grep -E '[0-9]+\.[0-9]+\.[0-9]+' -o)"
     echo "${PYVENV_PREFIX}venv ${PYTHON_VER}${PYVENV_SUFFIX}"
   else
     echo "${PYVENV_NOVENV}"
